1fd011f2bb914ff1f41591e94e6320a0c4fa1c16
Сергей Ефимочкин <Sergeyanton2007@gmail.com>
2018-12-12 21:48:14 +0300

    initial commit

---
+++ B/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "main.h"
+
+int main() {
+    call_print();
+    return 0;
+}
---
+++ B/main.h
@@ -0,0 +1,10 @@
+//
+// Created by Сергей Ефимочкин on 2018-12-12.
+//
+#include "A/lib.h"
+
+void call_print(){
+    print_smth();
+}
+
+
